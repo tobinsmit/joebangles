e50558e778cd8be157e1056c3e5d6db55933aff4
Tobin Smit <tobinksmit@gmail.com>
2018-09-05 16:56:59 +1000

    Initialised firebase functions

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,35 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.myFunctionName = functions.firestore.document('courses/requests').onWrite((change, context) => {
+    // ... Your code here
+    console.log(change.after.data());
+    request('https://www.handbook.unsw.edu.au/undergraduate/specialisations/2019/mtrnah', function (error, response, html) {
+      if (!error && response.statusCode === 200) {
+        var $ = cheerio.load(html);
+        $('#structure > div > .a-card').each(function(i, element){
+          // var a = $(this).prev();
+          console.log(this);
+          console.log($(this).html);
+          console.log($(this).text);
+        });
+      } else {
+        console.log(error);
+      }
+    });
+
+});
+
+
+// require("jsdom").env("", function(err, window) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+ 
+//     var $ = require("jquery")(window);
 // });
+
