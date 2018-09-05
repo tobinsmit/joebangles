af31d8124add782a4d1bc1a729c00a6ce11bcefc
Tobin Smit <tobinksmit@gmail.com>
2018-09-06 00:46:39 +1000

    Functions requests handbook and prints levels

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,26 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
 var request = require('request');
 var cheerio = require('cheerio');
 
 exports.myFunctionName = functions.firestore.document('courses/requests').onWrite((change, context) => {
-    // ... Your code here
     console.log(change.after.data());
+
     request('https://www.handbook.unsw.edu.au/undergraduate/specialisations/2019/mtrnah', function (error, response, html) {
       if (!error && response.statusCode === 200) {
         var $ = cheerio.load(html);
-        $('#structure > div > .a-card').each(function(i, element){
-          // var a = $(this).prev();
-          console.log(this);
-          console.log($(this).html);
-          console.log($(this).text);
+
+        // Loop through levels
+        $('#structure .m-accordion').each(function(i, level){
+          title = $(level).prev().text().trim();
+          console.log(title);
+          });
         });
       } else {
         console.log(error);
       }
     });
 
+    return 1
 });
-
-
-// require("jsdom").env("", function(err, window) {
-//     if (err) {
-//         console.error(err);
-//         return;
-//     }
- 
-//     var $ = require("jquery")(window);
-// });
 
