0b2ab8f2f0dcec63c0f4b802faf8b9c321f60deb
alexair1 <alexair@optusnet.com.au>
2018-09-06 21:09:51 +1000

    Added dropzone highlighting
    When an element is dragged, the dropzones that is allowed to be dropped in with highlight

--- website/js/dragdrop.js
+++ website/js/dragdrop.js
@@ -3,8 +3,32 @@ var dragset = dragula({
         return el.classList.contains('draggable-container');
     },
     accepts: function(el, target, source, sibling){
+
+    	if($(el).attr('id') == 'draggable1'){
+    		return target.classList.contains('group1');
+    	} else if($(el).attr('id') == 'draggable2'){
+    		return target.classList.contains('group2');
+    	} else {
+    		return false
+    	}
     // Any logic can go here that decides what element 'el' can be dropped into what target 'target'.
     // Documentation: https://github.com/bevacqua/dragula
-    return true;
     }
 });
+
+// This function is triggered when an element 'el' from source 'source' is dragged
+dragset.on('drag', function(el, source){
+
+	if($(el).attr('id') == 'draggable1'){
+		$('.group1').css("background-color","blue");
+    } else if($(el).attr('id') == 'draggable2'){
+    	$('.group2').css("background-color","blue");
+    } 
+
+});
+
+// This function is triggered when an element 'el' is dropped
+dragset.on('dragend', function(el){
+	$('.draggable-container').css("background-color","white");
+});
+	
