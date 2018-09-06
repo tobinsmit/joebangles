61b353c59dcd57fcf470e9b69715e45a9f87e5dc
Tobin Smit <tobinksmit@gmail.com>
2018-09-06 15:37:51 +1000

    Rewrote scrapeSpec after testing locally

--- functions/index.js
+++ functions/index.js
@@ -3,45 +3,50 @@ const functions = require('firebase-functions');
 var request = require('request');
 var cheerio = require('cheerio');
 
-exports.myFunctionName = functions.firestore.document('courses/requests').onWrite((change, context) => {
+exports.scrapeSpec = functions.firestore.document('degreeScrapeRequests/{specid}').onWrite((change, context) => {
+    
     console.log(change.after.data());
+    specid = context.params.specid;
+    console.log("specid: " + specid);
 
-    request('https://www.handbook.unsw.edu.au/undergraduate/specialisations/2019/mtrnah', function (error, response, html) {
+    doc = {}
+
+    request('https://www.handbook.unsw.edu.au/undergraduate/specialisations/2019/' + specid, function(error, response, html) {
       if (!error && response.statusCode === 200) {
         var $ = cheerio.load(html);
 
-        data = {}
+        doc["title"] = $("#subject-intro h2 span").text();
+        doc["courseLevels"] = {};
 
         // Loop through levels
-        $('#structure .m-accordion').each(function(i, level){
+        $('#structure .m-accordion').each( function(i, level){
           levelid = $(level).prev().text().trim();
-          console.log(levelid);
-          data[levelid] = {}
-          data[levelid]["compulsory"] = {};
-          data[levelid]["optionSets"] = [];
+          doc["courseLevels"][levelid] = {}
+          doc["courseLevels"][levelid]["compulsory"] = {};
+          doc["courseLevels"][levelid]["optionSets"] = [];
 
           // Loop through each sub level (compulsory then options)
-          $(level).find('.o-course-list').each( (i, sublevel) => {
-            if (i == 0) {
+          $(level).find('.o-course-list').each( function(i, sublevel) {
+            if (i === 0) {
               // Compulsory : first sublevel
 
-              $(sublevel).find('span.align-left').each( (i, course) => {
+              $(sublevel).find('span.align-left').each( function(i, course) {
                 courseid = $(course).text();
                 courseLongname = $(course).parent().next().text().trim();
-                data[levelid]["compulsory"][courseid] = courseLongname;
+                doc["courseLevels"][levelid]["compulsory"][courseid] = {"longname" : courseLongname};
               });
             } else {
               // Options : >= 2nd sublevel
 
-              optionSetData = {}; // For some reason "data[levelid]["optionSets"][i-1][courseid] = courseLongname" didn't work but this way does.
+              optionSetData = {}; // For some reason "doc["courseLevels"][levelid]["optionSets"][i-1][courseid] = courseLongname" didn't work but this way does.
 
               $(sublevel).find('span.align-left').each( (i, course) => {
                 courseid = $(course).text();
                 courseLongname = $(course).parent().next().text().trim();
-                optionSetData[courseid] = courseLongname;
+                optionSetData[courseid] = {"longname" : courseLongname};
               });
 
-              data[levelid]["optionSets"][i-1] = optionSetData; // needs to shift i by -1 because of the compulsory sublevel
+              doc["courseLevels"][levelid]["optionSets"][i-1] = optionSetData; // needs to shift i by -1 because of the compulsory sublevel
 
             }
            
@@ -49,14 +54,40 @@ exports.myFunctionName = functions.firestore.document('courses/requests').onWrit
          
         }); // End level loop
 
-        console.log(data);
+        console.log(doc);
 
       } else {
-        console.log(error);
+        console.error("ERROR with REQUEST. error != null or status != 200");
+        console.error(error);
+        return null
       }
 
     }); // End request
 
-    return 1
+    return change.after.ref.set({scrape: doc}, {merge: true});
+
 });
 
+// Test Data
+/*
+scrapeSpec(
+  {
+    before: {
+      degrees: {
+        MECHAH : true
+      }
+    },
+    after: {
+      degrees: {
+        MECHAH : true
+      },
+      degreeScrapeRequests: {
+        CHEMB1 : {
+          time : 10000
+        }
+      }
+    }
+  },
+  {params: {specid: 'CHEMB1'}}
+)
+*/
