b4d5a60ceec5392ca35c5f1964da32f1948f530a
Tobin Smit <tobinksmit@gmail.com>
2018-09-19 13:10:49 +1000

    More react experiments

---
+++ react_exp/jb2/src/components/SpecControl.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SpecControl extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
--- react_exp/jb2/src/index.js
+++ react_exp/jb2/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import DragDropPlan from './components/DragDropPlan';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <div>
+    <DragDropPlan />
+  </div>
+  ,
+  document.getElementById('root')
+);
