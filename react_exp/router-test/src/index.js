fb083647e05249fb50fa19cd92ff1fd571839cf9
Tobin Smit <tobinksmit@gmail.com>
2018-09-19 11:59:53 +1000

    Started router testing

---
+++ react_exp/router-test/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {} form 'react-router-dom';
+
+// ========================================
+
+function App(props) {
+  return (
+    <button>
+      herro
+    </button>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
