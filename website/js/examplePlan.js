89b49a74df7e884387bb95a6280f212a1ffd9242
Tobin Smit <tobinksmit@gmail.com>
2018-09-20 21:28:58 +1000

    Commented unnecessary objects

--- website/js/examplePlan.js
+++ website/js/examplePlan.js
@@ -208,130 +208,130 @@ addCourse = function(course, location) {
 	$(location).append(html);
 }
 
-examplePlanArr = [
-	{
-	  courseid : "ENGG1000",
-	  availableTerms : [1,2,3],
-		longname : "Engineering Design",
-		completed : true
-	},
-	{
-	  courseid : "MATH1131",
-	  availableTerms : [1,2,3],
-		longname : "Mathematics 1A",
-		completed : true
-	},
-	{
-	  courseid : "MATH1231",
-	  availableTerms : [1,2,3],
-		longname : "Mathematics 1B",
-		prereq : "MATH1131 + MATH1141",
-		completed : true
-	},
-	{
-	  courseid : "PHYS1121",
-		longname : "Physics 1A",
-		availableTerms : ["Summer", "1", "2", "3"],
-		completed : true
-	},
-	{
-	  courseid : "MMAN2100",
-		longname : "Engineering Design 2",
-		availableTerms : [3],
-		// prereq : "ENGG1000",
-		completed : true
-	},
-	{
-	  courseid : "MMAN2300",
-		longname : "Engineering Mechanics 2",
-		availableTerms : ["2"],
-		prereq : "(CVEN1300 + MINE1300 + MMAN1300) * MATH2019 + MATH2018 + MATH2111 * MATH2221 + MATH2011 * MATH2121 + MATH2069 * MATH2121"
-	},
-	{
-	  courseid : "ELEC1111",
-		longname : "Electrical and Telecommunications Engineering",
-		availableTerms : [1,2]
-	},
-	{
-	  courseid : "MATH2019",
-		longname : "Engineering Mathematics 2E",
-		availableTerms : ["1", "3"],
-		prereq : "MATH1231 + MATH1241 + MATH1251"
-	},
-	{
-	  courseid : "MMAN2130",
-		longname : "Design and Manufacturing",
-		availableTerms : ["2", "3"]
-	},
-	{
-	  courseid : "MMAN1300",
-		longname : "Engineering Mechanics",
-		availableTerms : ["Summer", "2", "3"], // Made up
-		prereq : "(MATH1131 + MATH1141) * (PHYS1121 + PHYS1131 + PHYS1141)"
-	},
-	{
-	  courseid : "MMAN2400",
-		longname : "Mechanics of Solids 1",
-		availableTerms : ["1", "2"], // Made up
-		prereq : "(MATH1231 + MATH1241) * (MMAN1300 + CVEN1300 + MINE1300)"
-	}
-]
+// examplePlanArr = [
+// 	{
+// 	  courseid : "ENGG1000",
+// 	  availableTerms : [1,2,3],
+// 		longname : "Engineering Design",
+// 		completed : true
+// 	},
+// 	{
+// 	  courseid : "MATH1131",
+// 	  availableTerms : [1,2,3],
+// 		longname : "Mathematics 1A",
+// 		completed : true
+// 	},
+// 	{
+// 	  courseid : "MATH1231",
+// 	  availableTerms : [1,2,3],
+// 		longname : "Mathematics 1B",
+// 		prereq : "MATH1131 + MATH1141",
+// 		completed : true
+// 	},
+// 	{
+// 	  courseid : "PHYS1121",
+// 		longname : "Physics 1A",
+// 		availableTerms : ["Summer", "1", "2", "3"],
+// 		completed : true
+// 	},
+// 	{
+// 	  courseid : "MMAN2100",
+// 		longname : "Engineering Design 2",
+// 		availableTerms : [3],
+// 		// prereq : "ENGG1000",
+// 		completed : true
+// 	},
+// 	{
+// 	  courseid : "MMAN2300",
+// 		longname : "Engineering Mechanics 2",
+// 		availableTerms : ["2"],
+// 		prereq : "(CVEN1300 + MINE1300 + MMAN1300) * MATH2019 + MATH2018 + MATH2111 * MATH2221 + MATH2011 * MATH2121 + MATH2069 * MATH2121"
+// 	},
+// 	{
+// 	  courseid : "ELEC1111",
+// 		longname : "Electrical and Telecommunications Engineering",
+// 		availableTerms : [1,2]
+// 	},
+// 	{
+// 	  courseid : "MATH2019",
+// 		longname : "Engineering Mathematics 2E",
+// 		availableTerms : ["1", "3"],
+// 		prereq : "MATH1231 + MATH1241 + MATH1251"
+// 	},
+// 	{
+// 	  courseid : "MMAN2130",
+// 		longname : "Design and Manufacturing",
+// 		availableTerms : ["2", "3"]
+// 	},
+// 	{
+// 	  courseid : "MMAN1300",
+// 		longname : "Engineering Mechanics",
+// 		availableTerms : ["Summer", "2", "3"], // Made up
+// 		prereq : "(MATH1131 + MATH1141) * (PHYS1121 + PHYS1131 + PHYS1141)"
+// 	},
+// 	{
+// 	  courseid : "MMAN2400",
+// 		longname : "Mechanics of Solids 1",
+// 		availableTerms : ["1", "2"], // Made up
+// 		prereq : "(MATH1231 + MATH1241) * (MMAN1300 + CVEN1300 + MINE1300)"
+// 	}
+// ]
 
-// loadPlanArr(examplePlanArr)
+// // loadPlanArr(examplePlanArr)
 
 
-examplePlanObj = {
-	completedCourses : {
-		"ENGG1000" : {
-			longname : "Engineering Design"
-		},
-		"MATH1131" : {
-			longname : "Mathematics 1A"
-		},
-		"MATH1231" : {
-			longname : "Mathematics 1B",
-			prereq : "MATH1131 + MATH1141"
-		}
-	},
-	plannedCourses : {
-		"PHYS1121" : {
-			longname : "Physics 1A",
-			availableTerms : ["Summer Term", "Term 1", "Term 2", "Term 3"],
-			chosenTerm : "Term 1"
-		},
-		"MMAN2100" : {
-			longname : "Engineering Design 2",
-			availableTerms : ["Term 3"],
-			prereq : "ENGG1000"
-		},
-		"MMAN2300" : {
-			longname : "Engineering Mechanics 2",
-			availableTerms : ["Term 2"],
-			prereq : "(CVEN1300 + MINE1300 + MMAN1300) * MATH2019 + MATH2018 + MATH2111 * MATH2221 + MATH2011 * MATH2121 + MATH2069 * MATH2121"
-		},
-		"ELEC1111" : {
-			longname : "Electrical and Telecommunications Engineering",
-			terms : ["Term 1", "Term 3"]
-		},
-		"MATH2019" : {
-			longname : "Engineering Mathematics 2E",
-			terms : ["Term 1", "Term 3"],
-			prereq : "MATH1231 + MATH1241 + MATH1251"
-		},
-		"MMAN2130" : {
-			longname : "Design and Manufacturing",
-			terms : ["Term 2", "Term 3"]
-		},
-		"MMAN1300" : {
-			longname : "Engineering Mechanics",
-			availableTerms : ["Summer Term", "Term 2", "Term 3"], // Made up
-			prereq : "(MATH1131 + MATH1141) * (PHYS1121 + PHYS1131 + PHYS1141)"
-		},
-		"MMAN2400" : {
-			longname : "Mechanics of Solids 1",
-			terms : ["Term 1", "Term 2"], // Made up
-			prereq : "(MATH1231 + MATH1241) * (MMAN1300 + CVEN1300 + MINE1300)"
-		}
+// examplePlanObj = {
+// 	completedCourses : {
+// 		"ENGG1000" : {
+// 			longname : "Engineering Design"
+// 		},
+// 		"MATH1131" : {
+// 			longname : "Mathematics 1A"
+// 		},
+// 		"MATH1231" : {
+// 			longname : "Mathematics 1B",
+// 			prereq : "MATH1131 + MATH1141"
+// 		}
+// 	},
+// 	plannedCourses : {
+// 		"PHYS1121" : {
+// 			longname : "Physics 1A",
+// 			availableTerms : ["Summer Term", "Term 1", "Term 2", "Term 3"],
+// 			chosenTerm : "Term 1"
+// 		},
+// 		"MMAN2100" : {
+// 			longname : "Engineering Design 2",
+// 			availableTerms : ["Term 3"],
+// 			prereq : "ENGG1000"
+// 		},
+// 		"MMAN2300" : {
+// 			longname : "Engineering Mechanics 2",
+// 			availableTerms : ["Term 2"],
+// 			prereq : "(CVEN1300 + MINE1300 + MMAN1300) * MATH2019 + MATH2018 + MATH2111 * MATH2221 + MATH2011 * MATH2121 + MATH2069 * MATH2121"
+// 		},
+// 		"ELEC1111" : {
+// 			longname : "Electrical and Telecommunications Engineering",
+// 			terms : ["Term 1", "Term 3"]
+// 		},
+// 		"MATH2019" : {
+// 			longname : "Engineering Mathematics 2E",
+// 			terms : ["Term 1", "Term 3"],
+// 			prereq : "MATH1231 + MATH1241 + MATH1251"
+// 		},
+// 		"MMAN2130" : {
+// 			longname : "Design and Manufacturing",
+// 			terms : ["Term 2", "Term 3"]
+// 		},
+// 		"MMAN1300" : {
+// 			longname : "Engineering Mechanics",
+// 			availableTerms : ["Summer Term", "Term 2", "Term 3"], // Made up
+// 			prereq : "(MATH1131 + MATH1141) * (PHYS1121 + PHYS1131 + PHYS1141)"
+// 		},
+// 		"MMAN2400" : {
+// 			longname : "Mechanics of Solids 1",
+// 			terms : ["Term 1", "Term 2"], // Made up
+// 			prereq : "(MATH1231 + MATH1241) * (MMAN1300 + CVEN1300 + MINE1300)"
+// 		}
 
-	}
-}
+// 	}
+// }
